b88b92520f74e6954cacbb6ea00f5988a298fd8c
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2024-07-02 08:30:59 -0300

    feat: work execCarrosNaSerra

--- src/scrappers/carros-na-serra/index.ts
+++ src/scrappers/carros-na-serra/index.ts
@@ -1,42 +1,113 @@
 import chalk from "chalk";
 import logUpdate from "log-update";
 import type { Browser } from "puppeteer";
-import { autoScroll } from "../../utils/autoscroll";
+import { logger } from "../../utils/logger";
 import { generateRandomUA } from "../../utils/random-ua-generator";
+import type { AdvertisementDTO } from "../../entities/AdvertisementDTO";
 
-export async function execCarrosNaSerra(browser: Browser) {
+export async function execCarrosNaSerra(browser: Browser): Promise<AdvertisementDTO[]> {
     logUpdate(chalk.yellow("[0/?] Starting execCarrosNaSerra"));
 
-    logUpdate(chalk.yellow("[1/?] Creating browser page"));
     const page = await browser.newPage();
-    logUpdate(chalk.green("[1/?] Page was created"));
-
-    logUpdate(chalk.yellow("[2/?] Setting User Agent and Navigation timeout"));
-    page.setDefaultNavigationTimeout(60000);
     await page.setUserAgent(generateRandomUA());
-    logUpdate(chalk.green("[2/?] Setted User Agent and Navigation timeout"));
 
-    logUpdate(chalk.green("[3/?] Entering https://www.carrosnaserra.com.br"));
+    logger('start', '[3/?] Entering https://www.carrosnaserra.com.br');
     await page.goto("https://www.carrosnaserra.com.br/capa", { waitUntil: 'networkidle2' });
-    logUpdate(chalk.green("[3/?] Entered https://www.carrosnaserra.com.br"));
+    logger('success', '[3/?] Entered https://www.carrosnaserra.com.br');
 
-    logUpdate(chalk.green("[4/?] Closing 'fechar' section"));
-    await page.waitForSelector(".fechar");
-    await page.click(".fechar");
-    logUpdate(chalk.green("[4/?] Closed 'fechar' section"));
+    const categorias = ['CARROS'];
+    const intervalos = [
+        { min: 3000, max: 10000 },
+        // Descomente os intervalos conforme necessário
+        { min: 10000, max: 20000 },
+        { min: 20000, max: 30000 },
+        { min: 30000, max: 40000 },
+        { min: 40000, max: 50000 },
+        { min: 50000, max: 60000 },
+        { min: 60000, max: 70000 },
+        { min: 70000, max: 80000 },
+        { min: 80000, max: 90000 },
+        { min: 90000, max: 100000 },
+        { min: 100000, max: 150000 },
+        { min: 150000, max: 200000 },
+        { min: 200000, max: 10000000 }
+    ];
 
-    logUpdate(chalk.green("[5/?] appling search filters"));
-    await page.select('select[name="categoria"]', 'CARROS');
-    await page.type('input[name="de"]', '5.000');
-    await page.type('input[name="ate"]', '100.000');
-    await page.select('select[name="regiao"]', 'ALTOS DA SERRA');
-    logUpdate(chalk.green("[5/?] Applied search filters"));
+    const result: AdvertisementDTO[] = [];
 
-    await autoScroll(page);
+    for (const categoria of categorias) {
+        for (const intervalo of intervalos) {
+            logger('info', `[?/?] Searching ${categoria} from R$ ${intervalo.min} to R$ ${intervalo.max}`);
 
-    logUpdate(chalk.blue("[Completed] Finished execution of execCarrosNaSerra"))
-    logUpdate.done();
+            await page.select('select[name="categoria"]', categoria);
 
-    return
+            await page.$eval('input[name="de"]', el => el.value = '');
+            await page.$eval('input[name="ate"]', el => el.value = '');
+            await page.type('input[name="de"]', intervalo.min.toString(), { delay: 100 });
+            await page.type('input[name="ate"]', intervalo.max.toString(), { delay: 100 });
+
+            await Promise.all([
+                page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 }),
+                page.click('.btbuscar')
+            ]);
+
+            do {
+                const anuncios = await page.evaluate(() => {
+                    const items = Array.from(document.querySelectorAll("a > div.estoque-card"));
+                    return items.map(item => {
+                        const name = item.querySelector("div.modelo-estoque-card")?.textContent?.trim() ?? 'Nome não informado';
+                        const brand = item.querySelector("div.ano-card")?.textContent?.trim() ?? 'Marca não informada';
+
+                        const yearKmText = item.querySelector("div.ano-cor-km-card")?.textContent?.trim() ?? 'Ano e quilometragem não informados';
+                        const [kmText, yearText] = yearKmText.split('\n').map(text => text.trim());
+                        const km = kmText.includes('km não informado') ? 'Quilometragem não informada' : kmText;
+                        let year = yearText ?? 'Ano não informado';
+                        if (year.endsWith('-')) {
+                            year = year.slice(0, -1).trim();
+                        }
+
+                        const price = item.querySelector("div.preco-estoque-card")?.textContent?.trim() ?? 'Preço não informado';
+                        const linkElement = item.closest('a');
+                        let link = (linkElement ? linkElement.getAttribute('href') : '') ?? "Link não encontrado";
+                        if (link.startsWith('..')) {
+                            link = link.replace('..', '');
+                        }
+                        const fullLink = "https://www.carrosnaserra.com.br" + link;
+                        let location = link.split('/')[1] ?? 'Localização não informada';
+                        location = location.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+                        const image = item.querySelector("div.box-img-card > img")?.getAttribute('src') ?? 'Imagem não informada';
+                        const store = "Carros na Serra";
+                        return { brand: brand, name: `${brand} ${name}`, price, link: fullLink, location, store, image, year, km };
+                    });
+                });
+
+                console.log(anuncios);
+
+                result.push(...anuncios);
+
+                logger('success', `[?/?] Found ${anuncios.length} ads in range R$ ${intervalo.min} to R$ ${intervalo.max}`);
+
+                // Aguardar brevemente para o link 'Próxima' tornar-se clicável, se existir
+                const nextAvailable = await page.evaluate(() => {
+                    const nextLink = Array.from(document.querySelectorAll('a')).find(el => el.textContent?.includes('Próxima'));
+                    if (nextLink) {
+                        nextLink.click();
+                        return true;
+                    }
+                    return false;
+                });
+
+                if (nextAvailable) {
+                    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 });
+                } else {
+                    break; // Sai do loop se não houver mais páginas
+                }
+            } while (true); // Loop infinito, sai apenas pelo 'break'
+        }
+    }
+
+    logger('done', 'execCarrosNaSerra finished successfully');
+    console.log(result);
+
+    return result;
 }
-
