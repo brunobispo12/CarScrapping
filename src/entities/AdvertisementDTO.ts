9204cbdc07946b6fd2c27f6cafe4b471d661e0ae
Bruno Bispo <124530740+brunobispo12@users.noreply.github.com>
2024-07-02 08:30:35 -0300

    feat: add new fields in advertisementDTO

--- src/entities/AdvertisementDTO.ts
+++ src/entities/AdvertisementDTO.ts
@@ -1,8 +1,11 @@
 export type AdvertisementDTO = {
     name: string
     price: string
+    year: string
+    km: string
     location: string
     image: string
     link: string
     store: string
+    brand: string
 }
